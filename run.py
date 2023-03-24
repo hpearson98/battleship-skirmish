6a7c848b3d261f61b14cf4128c72ab877f29cf5f
hpearson98 <harry.m.pearson@gmail.com>
2023-03-24 19:30:04 +0000

    Added function that greets the player and confirms that they are ready to play

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def greeting():
+    """
+    Greets player when the game is initially run.
+    Asks the player for their name.
+    """
+    user_name = input("Welcome to Battleship Skirmish!\nWhat is your Name? ")
+    
+    def player_ready():
+        """
+        Confirms that the player is ready to play.
+        """
+        player_ready_choice = input(f"Hello {user_name}, if you are ready to play press y. ")
+        if player_ready_choice.lower() == "y":
+            print("Okay, let's play!")
+        else:
+            player_ready()
+
+    player_ready()
+
+greeting()
