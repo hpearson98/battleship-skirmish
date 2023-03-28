a883f9afc1db893ad402903308b3ec1dfc174183
hpearson98 <harry.m.pearson@gmail.com>
2023-03-28 20:41:54 +0000

    Added Board method to assign ships to the board

--- run.py
+++ run.py
@@ -1,10 +1,32 @@
+import random 
+
+class Board:
+    def __init__(self, size, num_ships, type):
+        self.size = [["*" for x in range(size)] for y in range(size)]
+        self.num_ships = ["@" for x in range(num_ships)]
+        self.type = type
+    
+    def assign_ships(self):
+        """
+        Gets a random space on the board and place a ship in that space.
+        If that space already has a ship then it will choose another space
+        """
+        for ship in self.num_ships:
+            rand_index = [random.randint(0, 4),random.randint(0, 4)]
+            while self.size[rand_index[0]][rand_index[1]] == ship:
+                rand_index = [[random.randint(0, 4)],[random.randint(0, 4)]]
+            
+            self.size[rand_index[0]][rand_index[1]] = ship
+            
+        return self.size
+
 def greeting():
     """
     Greets player when the game is initially run.
     Asks the player for their name.
     """
     user_name = input("Welcome to Battleship Skirmish!\nWhat is your Name? ")
-    
+
     def player_ready():
         """
         Confirms that the player is ready to play.
@@ -17,4 +39,8 @@ def greeting():
 
     player_ready()
 
-greeting()
+#greeting()
+player_board = Board(5, 4, "player")
+computer_board = Board(5, 4, "computer")
+print(player_board.assign_ships())
+print(computer_board.assign_ships())
